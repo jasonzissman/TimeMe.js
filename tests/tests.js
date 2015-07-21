918182639b0749585f036fedd84bb0abece86956
Jason Zissman <jzissman@follett.com>
2015-07-21 04:04:46 -0500

    Refactoring ifvisible usage to contain it better within the app, adding unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -27,3 +27,38 @@ QUnit.test("startTimer() and stopTimer() keeps track of time spent.", function( 
 	assert.notOk(isNaN(actualTime), "Should be a number.");	
 	assert.ok(actualTime >= 0, "Should be greater than or equals to 0." );
 });
+
+QUnit.test("startTimer() and stopTimer() keeps track of time spent on specific pages.", function( assert ) {
+	TimeMe.setCurrentPageName("first page");
+	TimeMe.startTimer();
+	TimeMe.stopTimer();
+	
+	TimeMe.setCurrentPageName("second page");
+	TimeMe.startTimer();
+	TimeMe.stopTimer();	
+	
+	var actualTimes = TimeMe.getTimeOnAllPagesInSeconds();
+	var expectedNumberOfEntries = 2;
+	assert.equal(actualTimes.length, expectedNumberOfEntries, "Should have two entries, one for each page" );
+	
+	assert.equal(actualTimes[0].pageName, "first page", "First response should be first page");
+	assert.equal(actualTimes[1].pageName, "second page", "Second response should be second page");
+});
+
+QUnit.test("resetAllRecordedPageTimes() should clear out all times.", function( assert ) {
+	TimeMe.setCurrentPageName("first page");
+	TimeMe.startTimer();
+	TimeMe.stopTimer();
+	
+	TimeMe.setCurrentPageName("second page");
+	TimeMe.startTimer();
+	TimeMe.stopTimer();	
+	
+	var actualTimes = TimeMe.getTimeOnAllPagesInSeconds();
+	assert.equal(actualTimes.length, 2, "Should have two entries." );
+	
+	TimeMe.resetAllRecordedPageTimes();
+	var actualTimes = TimeMe.getTimeOnAllPagesInSeconds();
+	var expectedNumberOfEntries = 0;
+	assert.equal(actualTimes.length, expectedNumberOfEntries, "Should have no entries since we cleared them out." );
+});

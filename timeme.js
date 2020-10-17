58e73435aef3717e0f1e69fa8cdab3ee5298c673
Jason Z <jason.zissman@gmail.com>
2020-10-17 18:18:54 -0500

    Updating License years

--- timeme.js
+++ timeme.js
@@ -1,4 +1,4 @@
-/*Copyright (c) 2019 Jason Zissman
+/*Copyright (c) 2020 Jason Zissman
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to
